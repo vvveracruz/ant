4a39440a9c36a8f9d9142268db3670fd860edb82
vvveracruz <login@vgg.cz>
2022-01-23 13:44:31 +0000

    chebyshev up to func, new notebooks

--- ant/__init__.py
+++ ant/__init__.py
@@ -1,2 +1,2 @@
 from ant.main import is_prime
-from .main import nth_prime_number, is_prime
+from .main import nth_prime_number, is_prime, chebyshev_theta_upto
--- ant/main.py
+++ ant/main.py
@@ -1,3 +1,6 @@
+import pandas as _pd
+import numpy as _np
+
 def nth_prime_number(n):
     if n==1:
         return 2
@@ -16,3 +19,17 @@ def is_prime(num):
              return False
         factor +=1
     return True
+
+def chebyshev_theta_upto(n):
+    '''
+    Returns a pandas series with all the values of chebyshev's theta function
+    up to n.
+
+    TODO: change range steps to vary with size of n to stop it from loading 
+    forever for big n.
+    '''
+    df=_pd.DataFrame(index=range(0, n, 1))
+    df['is_prime']=df.index.map(lambda n: n if is_prime(n) else 0)
+    df['log']=df.is_prime.map(lambda n: _np.log(n) if n!=0 else 0)
+    df['chebyshev_theta']=df.log.cumsum()
+    return df['chebyshev_theta']
