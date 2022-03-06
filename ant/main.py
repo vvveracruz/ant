62eacbae4f9b40eaef6625ffb862b24ca7e79237
vvveracruz <login@vgg.cz>
2022-03-06 11:39:05 +0000

    coprime and primes less than function

--- ant/main.py
+++ ant/main.py
@@ -1,5 +1,6 @@
 import pandas as _pd
 import numpy as _np
+import math as _math
 
 def nth_prime_number(n):
     if n==1:
@@ -13,6 +14,7 @@ def nth_prime_number(n):
     return num
 
 def is_prime(num):
+    # TODO: might be faster to use _math.gcd?
     factor = 2
     while (factor * factor <= num):
         if num % factor == 0:
@@ -53,3 +55,18 @@ def modulo_sum_table(n, returns_neg=False):
     for i in R:
         df[str(i)]=df.index.map(lambda x: (x+i)%n)
     return df
+
+def primes_less_than(n):
+    # TODO: there's probably a better way to do this lol
+    primes=[]
+    for i in range(1, n):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+def coprimes_less_than(n):
+    coprimes=[]
+    for i in range(1, n):
+        if _math.gcd(i, n)==1:
+            coprimes.append(i)
+    return coprimes
